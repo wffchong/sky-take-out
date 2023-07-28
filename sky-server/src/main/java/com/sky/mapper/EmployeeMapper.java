618136d60ca79ca7250ec01113ed3c9791f65651
wff <283953455@qq.com>
2023-07-28 16:27:18 +0800

    fix: 修改新增员工sql

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -17,7 +17,7 @@ public interface EmployeeMapper {
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
 
-    @Insert("insert into employee (name, username, password, phone, sex, id_number, create_time, update_time, create_user, update_user) " +
+    @Insert("insert into employee (name, username, password, phone, sex, id_number, create_time, update_time, create_user, update_user, status) " +
             "VALUES " +
             "(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{createTime},#{updateTime},#{createUser},#{updateUser},#{status})")
     void insert(Employee employee);
