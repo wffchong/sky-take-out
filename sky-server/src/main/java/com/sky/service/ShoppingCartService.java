454034dce88ad1017708e57c3d15fcea2d945b6f
coder-wff <wffchong@163.com>
2023-09-11 23:45:25 +0800

    feat: 获取购物车数据

---
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -0,0 +1,9 @@
+package com.sky.service;
+
+import com.sky.entity.ShoppingCart;
+
+import java.util.List;
+
+public interface ShoppingCartService {
+    List<ShoppingCart> showShoppingCart();
+}
---
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -0,0 +1,30 @@
+package com.sky.service.impl;
+
+import com.sky.context.BaseContext;
+import com.sky.entity.ShoppingCart;
+import com.sky.mapper.ShoppingCartMapper;
+import com.sky.service.ShoppingCartService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class ShoppingCartServiceImpl implements ShoppingCartService {
+
+    @Autowired
+    ShoppingCartMapper shoppingCartMapper;
+
+    @Override
+    public List<ShoppingCart> showShoppingCart() {
+        //获取微信用户的id
+        Long userId = BaseContext.getCurrentId();
+        ShoppingCart shoppingCart = ShoppingCart.builder()
+                .userId(userId)
+                .build();
+        List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
+        return list;
+    }
+}
